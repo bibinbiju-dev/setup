855e63da336c869b77429583c4f78d9c65683beb
Bibin Biju <bibinbiju482@gmail.com>
2026-03-03 00:45:25 +0530

    last commit

---
+++ omarchy/themes/draculaloled/neovim.lua
@@ -0,0 +1,20 @@
+return {
+	-- {
+	-- 	"Mofiqul/dracula.nvim",
+	-- 	config = function()
+	-- 		require("dracula").setup({
+	-- 			italic_comment = true,
+	-- 			transparent_bg = false,
+	-- 		})
+	-- 	end,
+	-- },
+
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "dracula",
+			colorscheme_bg = "dark",
+			italic_comment = true,
+		},
+	},
+}
