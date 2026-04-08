48b104a6cb298934b6c77b728f7dda415e019292
bibin <bibinbiju482@gmail.com>
2026-04-08 10:24:39 +0530

    changes

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "NvChad/nvim-colorizer.lua", enabled = false },
-  { "3rd/image.nvim", enabled = false },
+  -- { "3rd/image.nvim", enabled = false },
   { "barrett-ruth/live-server.nvim", enabled = false },
   -- { "goolord/alpha-nvim", enabled = false },
   { "iamcco/markdown-preview.nvim", enabled = false },
